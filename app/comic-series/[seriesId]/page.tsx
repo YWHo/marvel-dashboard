d2714429a5362f7f11f8dac7360e725eb9b16cd9
Brian Ho <YWHo@users.noreply.github.com>
2026-08-28 16:42:12 +1200

    feat: add styled comic series details view

--- app/comic-series/[seriesId]/page.tsx
+++ app/comic-series/[seriesId]/page.tsx
@@ -1,6 +1,7 @@
 import { Navbar } from "@/app/components/Navbar";
 import { Footer } from "@/app/components/Footer";
 import { ComicIssuesInfoTableWrapper } from "@/app/components/ComicIssuesInfoTableWrapper";
+import { ComicSeriesDetails } from "@/app/components/ComicSeriesDetails";
 
 type PageProps = {
   params: Promise<{ seriesId: string }>;
@@ -8,10 +9,11 @@ type PageProps = {
 
 export default async function ComicSeriesToIssuesPage({ params }: PageProps) {
   const { seriesId } = await params;
-  
+
   return (
     <div className="container mx-auto p-2 font-(family-name:--font-geist-sans)">
       <Navbar />
+      <ComicSeriesDetails seriesId={seriesId} />
       <ComicIssuesInfoTableWrapper seriesId={seriesId} />
       <Footer />
     </div>
